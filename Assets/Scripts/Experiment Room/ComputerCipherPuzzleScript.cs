2a6dc0af859bc753d838f5f283d8ffc234f55085
Claire McGuigan <mcguigan.claire@outlook.com>
2025-02-20 16:59:13 -0500

    Adding folder content for photos and family info

--- Assets/Scripts/Experiment Room/ComputerCipherPuzzleScript.cs
+++ Assets/Scripts/Experiment Room/ComputerCipherPuzzleScript.cs
@@ -5,16 +5,37 @@ public class ComputerCipherPuzzleScript : MonoBehaviour
 {
     public GameObject openFolderDisplay;
     public TMP_Text headerText;
+    public TMP_Text folderContentText;
 
     public void OpenFolder(string folderName)
     {
         Debug.Log(folderName);
         headerText.text = folderName;
+
+        // Populating file content
+        if (folderName == "Photos")
+        {
+            folderContentText.text = "Folder Empty";
+        }
+        if (folderName == "Family Info")
+        {
+            folderContentText.text = "Date of Birth: September 15th, 1983 " +
+                "\nCity Born in: Columbus Ohio" +
+                "\nDoctor's Phone Number: 123-583-9013" +
+                "\nDoctor's Address: 580 Cherry Lane" +
+                "\nFather's Name: Jerome Pale " +
+                "\nWiFi Password: DaRwIn34!" +
+                "\nMother's Name: Lois Konstyl";
+        }
+
+
         openFolderDisplay.SetActive(true);
     }
 
     public void CloseFolder()
     {
+        folderContentText.text = "";
+        headerText.text = "";
         openFolderDisplay.SetActive(false);
     }
 }
