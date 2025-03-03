51b1ef11f61b5435b2660f2a7b9a6523a9329587
Claire McGuigan <mcguigan.claire@outlook.com>
2025-03-03 16:33:46 -0500

    displaying the different puzzles when different samples are tested

--- Assets/Scripts/Experiment Room/BloodTestingPuzzleScript.cs
+++ Assets/Scripts/Experiment Room/BloodTestingPuzzleScript.cs
@@ -3,28 +3,42 @@ using UnityEngine;
 public class BloodTestingPuzzleScript : MonoBehaviour
 {
     public GameObject magnifiedDisplay;
-    private GameObject[] magnifiedPuzzles;
+    public GameObject sampleA;
+    public GameObject sampleB;
+    public GameObject sampleC;
+    public GameObject sampleD;
 
-    void Start()
-    {
-        //magnifiedPuzzles = gameObject.GetComponentsInChildren<GameObject>();
-        //Debug.Log(magnifiedPuzzles);
-        // Loop through all children of the current GameObject
-        /*foreach (Transform child in magnifiedDisplay.transform)
-        {
-            Debug.Log("Child: " + child.name);
-        }*/
-        Transform specificChild = magnifiedDisplay.Find("Glass");
-    }
 
     public void TriggerFunction(string gameObjectName)
     {
-        Debug.Log("INSIDE: "+ gameObjectName);
         magnifiedDisplay.SetActive(true);
+
+        if (gameObjectName == "Blood Sample A")
+        {
+            sampleA.SetActive(true);
+        }
+        else if (gameObjectName == "Blood Sample B")
+        {
+            sampleB.SetActive(true);
+        }
+        else if (gameObjectName == "Blood Sample C")
+        {
+            sampleC.SetActive(true);
+        }
+        else
+        {
+            sampleD.SetActive(true);
+        }
     }
 
     public void CloseButtonClicked()
     {
         magnifiedDisplay.SetActive(false);
+
+        // ensure all the sample views are not active
+        sampleA.SetActive(false);
+        sampleB.SetActive(false);
+        sampleC.SetActive(false);
+        sampleD.SetActive(false);
     }
 }
