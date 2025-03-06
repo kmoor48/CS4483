e0eca0ae55f04f0e6935d2d5043065a8061c9c5d
Kaitlyn Moore <90728895+kmoor48@users.noreply.github.com>
2025-03-05 19:24:16 -0500

    add throw prompt text

--- Assets/Scripts/ThrowPowder.cs
+++ Assets/Scripts/ThrowPowder.cs
@@ -1,3 +1,44 @@
+//using UnityEngine;
+
+//public class ThrowPowder : MonoBehaviour
+//{
+//    public GameObject messageOnWall;
+//    public Transform player;
+//    public Transform wallPosition;
+//    public float throwDistance = 3.0f;
+
+//    void Update()
+//    {
+//        if (Input.GetKey(KeyCode.T))
+//        {
+//            Debug.Log("T was pressed!");
+
+//            float distance = Vector3.Distance(player.position, wallPosition.position);
+//            Debug.Log("Distance to wall: " + distance);
+
+//            if (distance <= throwDistance)
+//            {
+//                if (InventoryManager.Instance.HasItem("Foundation_Powder"))
+//                {
+//                    Debug.Log("Throwing Powder...");
+//                    InventoryManager.Instance.RemoveItem("Foundation_Powder");
+//                    InventoryManager.Instance.AddItem("Powder Used");
+//                    messageOnWall.SetActive(true);
+//                }
+//                else
+//                {
+//                    Debug.Log("You don't have the foundation powder!");
+//                }
+//            }
+//            else
+//            {
+//                Debug.Log("Too far from the wall!");
+//            }
+//        }
+//    }
+//}
+
+
 using UnityEngine;
 
 public class ThrowPowder : MonoBehaviour
@@ -5,34 +46,47 @@ public class ThrowPowder : MonoBehaviour
     public GameObject messageOnWall;
     public Transform player;
     public Transform wallPosition;
-    public float throwDistance = 3.0f;
+    public float throwDistance = 2.0f;
+    public GameObject throwText; 
+
+    private bool isNearWall = false;
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.T))
+        float distance = Vector3.Distance(player.position, wallPosition.position);
+
+        if (distance <= throwDistance)
+        {
+            if (!isNearWall) 
+            {
+                throwText.gameObject.SetActive(true);
+                isNearWall = true;
+            }
+        }
+        else
+        {
+            if (isNearWall)
+            {
+                throwText.gameObject.SetActive(false);
+                isNearWall = false;
+            }
+        }
+
+        if (isNearWall && Input.GetKeyDown(KeyCode.T))
         {
             Debug.Log("T was pressed!");
 
-            float distance = Vector3.Distance(player.position, wallPosition.position);
-            Debug.Log("Distance to wall: " + distance);
-
-            if (distance <= throwDistance)
+            if (InventoryManager.Instance.HasItem("Foundation_Powder"))
             {
-                if (InventoryManager.Instance.HasItem("Foundation_Powder"))
-                {
-                    Debug.Log("Throwing Powder...");
-                    InventoryManager.Instance.RemoveItem("Foundation_Powder");
-                    InventoryManager.Instance.AddItem("Powder Used");
-                    messageOnWall.SetActive(true);
-                }
-                else
-                {
-                    Debug.Log("You don't have the foundation powder!");
-                }
+                Debug.Log("Throwing Powder...");
+                InventoryManager.Instance.RemoveItem("Foundation_Powder");
+                InventoryManager.Instance.AddItem("Powder Used");
+                messageOnWall.SetActive(true);
+                throwText.gameObject.SetActive(false); 
             }
             else
             {
-                Debug.Log("Too far from the wall!");
+                Debug.Log("You don't have the foundation powder!");
             }
         }
     }
