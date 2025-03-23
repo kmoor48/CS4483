499b997e2af03d241e6ab7b91d47aa7d45658975
Claire McGuigan <mcguigan.claire@outlook.com>
2025-03-23 19:10:45 -0400

    working door code puzzle

--- Assets/Keypad/Scripts/Keypad.cs
+++ Assets/Keypad/Scripts/Keypad.cs
@@ -12,7 +12,7 @@ namespace NavKeypad
         [SerializeField] private UnityEvent onAccessGranted;
         [SerializeField] private UnityEvent onAccessDenied;
         [Header("Combination Code (9 Numbers Max)")]
-        [SerializeField] private int keypadCombo = 12345;
+        [SerializeField] private int keypadCombo = 0417;
 
         public UnityEvent OnAccessGranted => onAccessGranted;
         public UnityEvent OnAccessDenied => onAccessDenied;
