4ff25f488f3b873ae06479a60030f7fc49d2e83b
Kaitlyn Moore <90728895+kmoor48@users.noreply.github.com>
2025-04-12 14:42:16 -0400

    eliminate build error

--- Assets/Scripts/InventoryDisplay.cs
+++ Assets/Scripts/InventoryDisplay.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditorInternal.Profiling.Memory.Experimental;
 
 public class InventoryDisplay : MonoBehaviour
 {
